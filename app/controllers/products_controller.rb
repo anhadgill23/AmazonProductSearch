5835cb1174621c3401cec07bf4f8b897fb6c5dc8
Anhad Gill <anhad@frogplum.com>
2019-08-01 09:54:11 -0700

    Adds error handling for controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,16 +13,22 @@ class ProductsController < ApplicationController
     # make api call
     @scraper_data = Scraper.get_product_info(params[:product][:asin])
 
-    @product.name = @scraper_data[:name]
-    @product.category = @scraper_data[:category]
-    @product.rank = @scraper_data[:rank]
-    @product.dimensions = @scraper_data[:dimensions]
+    # error handling
+    if @scraper_data.include? 'webpage not found'
+      @product.errors.add(:asin, @scraper_data.parameterize.underscore.to_sym, message: @scraper_data)
+    elsif @scraper_data.include? 'server error'
+      @product.errors.add(:asin,@scraper_data.parameterize.underscore.to_sym, message: @scraper_data)
+    end
 
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Here is the product information.' }
-        format.json { render :show, status: :created, location: @product }
-      else
+    if @product.errors.empty?
+      @product.name = @scraper_data[:name]
+      @product.category = @scraper_data[:category]
+      @product.rank = @scraper_data[:rank]
+      @product.dimensions = @scraper_data[:dimensions]
+      @product.save
+      redirect_to @product, notice: 'Here is the product information.'
+    else
+      respond_to do |format|
         format.html { render :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -7,14 +7,26 @@ class Scraper
     headers = {
       "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/601.5.17 (KHTML, like Gecko) Version/9.1 Safari/601.5.17'
     }
-    Nokogiri::HTML(HTTParty.get(url, headers: headers))
+    response = HTTParty.get(url, headers: headers)
+    case response.code
+    when 200
+      Nokogiri::HTML(response)
+    when 404
+      return 'webpage not found'
+    when 500...600
+      return 'server error'
+    end
   end
 
   # specify URL to making API call to
   def self.get_product_info(asin)
     url = "https://www.amazon.com/dp/#{asin}"
     response = fetch_data(url)
-    return response if response.nil?
+    if response.include? 'webpage not found'
+      return response
+    elsif response.include? 'server error'
+      return response
+    end
 
     {
       name: name(response),
@@ -27,34 +39,39 @@ class Scraper
   # Search product name by CSS
   def self.name(data)
     name = data.css('#productTitle')
-    name.present? ? name.first.try(:children).try(:text).lstrip!.rstrip!.squish : nil
+    name.present? ? name.first.try(:children).try(:text).try(:lstrip!).try(:rstrip!).try(:squish) : nil
   end
 
   # Search for category by CSS
   def self.category(data)
     category = data.css('.a-color-tertiary')
-    category.present? ? category.first.try(:children).try(:text).lstrip!.rstrip!.squish : nil
+    category.present? ? category.first.try(:children).try(:text).try(:lstrip!).try(:rstrip!).try(:squish) : nil
   end
 
-  # Search for rank by CSS
+  # Search for rank by different CSS selectors
   def self.rank(data)
     if data.css('#SalesRank').present?
-      data.css('#SalesRank').first.try(:text).match(/(#)[\d,]+\sin\s[\w\s,&]+/)[0].rstrip!
+      rank = data.css('#SalesRank').first.try(:text).match(/(#)[\d,]+\sin\s[\w\s,&]+/)
+      rank.present? ? rank[0].rstrip! : nil
     elsif data.css('#prodDetails').present?
-      data.css('#prodDetails').try(:text).match(/(#)[\d,]+\sin\s[\w\s&]+/)[0].rstrip!
+      rank = data.css('#prodDetails').try(:text).match(/(#)[\d,]+\sin\s[\w\s&]+/)
+      rank.present? ? rank[0].rstrip! : nil
     elsif data.css('#descriptionAndDetails').present?
-      data.css('#descriptionAndDetails').match(/(#)[\d,]+\sin\s[\w\s&]+/)[0].rstrip!
+      rank = data.css('#descriptionAndDetails').match(/(#)[\d,]+\sin\s[\w\s&]+/)
+      rank.present? ? rank[0].rstrip! : nil
     end
   end
 
-  # Search for dimensions by CSS
+  # Search for dimensions by different CSS selectors
   def self.dimensions(data)
     if data.css('.size-weight+ .size-weight .value').present?
       data.css('.size-weight+ .size-weight .value').try(:children).try(:text)
     elsif data.css('#prodDetails').present?
-      data.css('#prodDetails').try(:text).match(/[\d\."']+\sx\s[\d\."']+\sx\s[\d\."']+\s\w+/)[0]
+      dimensions = data.css('#prodDetails').try(:text).match(/[\d\."']+\sx\s[\d\."']+\sx\s[\d\."']+\s\w+/)
+      dimensions.present? ? dimensions[0] : nil
     elsif data.css('#descriptionAndDetails').present?
-      data.css('#descriptionAndDetails').try(:text).match(/[\d\."']+\sx\s[\d\."']+\sx\s[\d\."']+\s\w+/)[0]
+      dimensions = data.css('#descriptionAndDetails').try(:text).match(/[\d\."']+\sx\s[\d\."']+\sx\s[\d\."']+\s\w+/)
+      dimensions.present? ? dimensions[0] : nil
     end
   end
 end
